45c5406ffc6cb61d6861443e71606ab843e933d6
amirali eyshi <newjob2020.2000@gmail.com>
2025-05-09 18:22:26 +0330

    add : cakecontroller adding my project

---
+++ FirstMyProject/Controller/CakeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMyProject.Models;
+
+namespace FirstMyProject.Controller
+{
+    public class CakeController : ControllerContext
+    {
+        private readonly ICakeRepository _cakeRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CakeController(ICakeRepository cakeRepository,ICategoryRepository categoryRepository)
+        {
+            _cakeRepository = cakeRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_cakeRepository.AllCake);
+        }
+
+        private IActionResult View(IEnumerable<Cake> allCake)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
