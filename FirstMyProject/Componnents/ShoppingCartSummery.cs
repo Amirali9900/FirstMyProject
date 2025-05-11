f94d0760d40f0b81fa74f1a1b76773d1a64c38b4
amirali eyshi <newjob2020.2000@gmail.com>
2025-05-11 07:00:40 +0330

    feat : add view shopping cart

---
+++ FirstMyProject/Componnents/ShoppingCartSummery.cs
@@ -0,0 +1,26 @@
+﻿using FirstMyProject.Models;
+using FirstMyProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMyProject.Componnents
+{
+    public class ShoppingCartSummery : ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummery(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
